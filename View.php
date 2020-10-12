f171d872af7911a4223ac49aabb7cfd26dd2fc1c
HendrikCromboom <hcromboom@gmail.com>
2020-10-12 21:41:25 +0200

    added pseudo code for the view

--- View.php
+++ View.php
@@ -1,1 +1,15 @@
 <?php
+
+// Display Player hand 2 cards
+
+// Display Opponent hand 1 out of 2 cards
+
+// Player has to hit, stand down or busts
+
+// Opponent displays 2 out of 2 cards
+
+// Opponent has to hit, stand down or busts
+
+// Winner is displayed
+
+// Next round option is displayed
